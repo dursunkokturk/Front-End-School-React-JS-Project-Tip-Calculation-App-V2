f91fae9dfbc45139c8d8a81506553eea492ced5f
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-05-16 09:00:59 +0300

    src klasoru icindeki app.jsx component icinde general-title div'i icinde ve container div'i icindeki left div'i icinde app.css dosyasinda css duzenlemesi yapildi

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,17 @@ export default function App() {
   return (
     <>
       <div className="general-title">
-        <h1>AYIRICI</h1>
+        <h1>BAHŞİŞ HESAPLAYICI</h1>
       </div>
       <div className="container">
         <div className="left">
-          <h4>Hesap</h4>
-          <input type="text" />
+          <div className="form-group">
+            <h4>Hesap</h4>
+            <div className="input-box">
+              <span className='icon'>₺</span>
+              <input type="text" placeholder='Ücret Giriniz'/>
+            </div>
+          </div>
         </div>
         <div className="right">
 
